b5c1263b711787bfdf630f79bfeea0fa7cee77f4
Warnier-zhang <warnier.zhang@gmail.com>
2019-09-12 10:05:12 +0800

    feat(Test): Encrypt text

---
+++ src/main/java/org/warnier/zhang/test/TestApplication.java
@@ -0,0 +1,11 @@
+package org.warnier.zhang.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+}
---
+++ src/main/java/org/warnier/zhang/test/controller/TestController.java
@@ -0,0 +1,15 @@
+package org.warnier.zhang.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.warnier.zhang.cip.util.StringUtils;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+    @GetMapping("/md5")
+    public String encryptText(String text) {
+        return StringUtils.md5(text);
+    }
+}
